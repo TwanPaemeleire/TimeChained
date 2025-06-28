11c135712b70bc2c65e0845def819edbfc10e801
Thomas Ketelaere <thomas.ketelaere@outlook.be>
2025-06-28 12:15:54 +0200

    Removed player aiming snapping

--- Assets/Scripts/PlayerWeaponRotateComponent.cs
+++ Assets/Scripts/PlayerWeaponRotateComponent.cs
@@ -11,17 +11,6 @@ public class PlayerWeaponRotateComponent : MonoBehaviour
     private Vector3 _currentPointRotateAround;
     private Vector3 _socketPos;
 
-    private Vector2[] snapDirections = new Vector2[]
-    {
-        new Vector2(1, 0),    
-        new Vector2(1, 1),    
-        new Vector2(0, 1),    
-        new Vector2(-1, 1),   
-        new Vector2(-1, 0),   
-        new Vector2(1, -1),   
-        new Vector2(-1, -1) 
-    };
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -34,36 +23,13 @@ public class PlayerWeaponRotateComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        //mousePos.z = 0f;
-
-        //Vector3 direction = (mousePos - transform.parent.position).normalized;
-        //transform.localPosition = direction * distanceFromPlayer;
-
-        //_shootComponent.SetDirection(direction);
-
         Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         mouseWorldPos.z = 0f;
 
         Vector3 dirToMouse = (mouseWorldPos - transform.parent.position).normalized;
+        float angle = Mathf.Atan2(dirToMouse.y, dirToMouse.x) * Mathf.Rad2Deg;
 
-        Vector2 bestDir = snapDirections[0];
-        float bestDot = Vector2.Dot(dirToMouse, snapDirections[0].normalized);
-
-        foreach (Vector2 snapDir in snapDirections)
-        {
-            float dot = Vector2.Dot(dirToMouse, snapDir.normalized);
-            if (dot > bestDot)
-            {
-                bestDot = dot;
-                bestDir = snapDir;
-            }
-        }
-
-        Vector3 snappedDirection = bestDir.normalized;
-        float angle = Mathf.Atan2(snappedDirection.y, snappedDirection.x) * Mathf.Rad2Deg;
-
-        if (snappedDirection.x < 0)
+        if (dirToMouse.x < 0)
         {
             _spriteRenderer.flipX = true;
             _socket.localPosition = new Vector3(-_socketPos.x, _socketPos.y);
@@ -77,19 +43,17 @@ public class PlayerWeaponRotateComponent : MonoBehaviour
 
         
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
-        _shootComponent.SetDirection(snappedDirection);
+        _shootComponent.SetDirection(dirToMouse);
     }
 
     public void FlipLeft()
     {
-        //_spriteRenderer.flipX = true;
         transform.localPosition = new Vector3(-_currentPointRotateAround.x, _currentPointRotateAround.y);
         _socket.localPosition = new Vector3(-_socketPos.x, _socketPos.y);
     }
 
     public void FlipRight()
     {
-        //_spriteRenderer.flipX = false;
         transform.localPosition = _currentPointRotateAround;
         _socket.localPosition = _socketPos;
     }
