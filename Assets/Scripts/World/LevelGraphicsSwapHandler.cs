cc1269b440325e2f20c68a278e3865acb78546d1
TwanPaemeleire <twan.paemeleire@gmail.com>
2025-06-29 00:45:50 +0200

    Added correct namespace to the level graphics swap handler.

--- Assets/Scripts/World/LevelGraphicsSwapHandler.cs
+++ Assets/Scripts/World/LevelGraphicsSwapHandler.cs
@@ -1,30 +1,33 @@
 using Assets.Scripts.World;
 using UnityEngine;
 
-public class LevelGraphicsSwapHandler : MonoBehaviour
+namespace Assets.Scripts.World
 {
-    [SerializeField] private GameObject _futureWorldObj;
-    [SerializeField] private GameObject _pastWorldObj;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class LevelGraphicsSwapHandler : MonoBehaviour
     {
-        _futureWorldObj.SetActive(WorldSwapHandler.Instance.IsInCyberpunkWorld);
-        _pastWorldObj.SetActive(!WorldSwapHandler.Instance.IsInCyberpunkWorld);
+        [SerializeField] private GameObject _futureWorldObj;
+        [SerializeField] private GameObject _pastWorldObj;
 
-        WorldSwapHandler.Instance.OnWorldSwap.AddListener(OnWorldSwap);
-        WorldSwapHandler.Instance.OnWorldFlicker.AddListener(OnWorldFlicker);
-    }
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _futureWorldObj.SetActive(WorldSwapHandler.Instance.IsInCyberpunkWorld);
+            _pastWorldObj.SetActive(!WorldSwapHandler.Instance.IsInCyberpunkWorld);
 
-    void OnWorldSwap()
-    {
-        _futureWorldObj.SetActive(WorldSwapHandler.Instance.IsInCyberpunkWorld);
-        _pastWorldObj.SetActive(!WorldSwapHandler.Instance.IsInCyberpunkWorld);
-    }
+            WorldSwapHandler.Instance.OnWorldSwap.AddListener(OnWorldSwap);
+            WorldSwapHandler.Instance.OnWorldFlicker.AddListener(OnWorldFlicker);
+        }
 
-    void OnWorldFlicker()
-    {
-        _futureWorldObj.SetActive(WorldSwapHandler.Instance.IsFlickeringInCyberpunkWorld);
-        _pastWorldObj.SetActive(!WorldSwapHandler.Instance.IsFlickeringInCyberpunkWorld);
+        void OnWorldSwap()
+        {
+            _futureWorldObj.SetActive(WorldSwapHandler.Instance.IsInCyberpunkWorld);
+            _pastWorldObj.SetActive(!WorldSwapHandler.Instance.IsInCyberpunkWorld);
+        }
+
+        void OnWorldFlicker()
+        {
+            _futureWorldObj.SetActive(WorldSwapHandler.Instance.IsFlickeringInCyberpunkWorld);
+            _pastWorldObj.SetActive(!WorldSwapHandler.Instance.IsFlickeringInCyberpunkWorld);
+        }
     }
 }
