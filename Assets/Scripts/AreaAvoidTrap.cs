a6369fff7501a52ebf10f86fd9feda54db3cbbd5
Samir-V <samirs.vertahovs@gmail.com>
2025-06-28 11:42:33 +0200

    Adjusted namespace in the area avoid trap

--- Assets/Scripts/AreaAvoidTrap.cs
+++ Assets/Scripts/AreaAvoidTrap.cs
@@ -1,41 +1,44 @@
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class AreaAvoidTrap : MonoBehaviour
+namespace Assets.Scripts
 {
-    [SerializeField] private float _damage = 2.0f;
-    [SerializeField] private float _delayBetweenHits = 1.0f;
-    Dictionary<HealthComponent, Coroutine> _entitiesInTrap = new Dictionary<HealthComponent, Coroutine>();
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    public class AreaAvoidTrap : MonoBehaviour
     {
-        if(collision.gameObject.TryGetComponent<HealthComponent>(out HealthComponent healthComponent))
+        [SerializeField] private float _damage = 2.0f;
+        [SerializeField] private float _delayBetweenHits = 1.0f;
+        Dictionary<HealthComponent, Coroutine> _entitiesInTrap = new Dictionary<HealthComponent, Coroutine>();
+
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            Debug.Log("ENTERED");
-            _entitiesInTrap.Add(healthComponent, StartCoroutine(TrapDamageCoroutine(healthComponent)));
+            if(collision.gameObject.TryGetComponent<HealthComponent>(out HealthComponent healthComponent))
+            {
+                Debug.Log("ENTERED");
+                _entitiesInTrap.Add(healthComponent, StartCoroutine(TrapDamageCoroutine(healthComponent)));
+            }
         }
-    }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.TryGetComponent<HealthComponent>(out HealthComponent healthComponent))
+        private void OnTriggerExit2D(Collider2D collision)
         {
-            Debug.Log("EXITED");
-            StopCoroutine(_entitiesInTrap[healthComponent]);
-            _entitiesInTrap.Remove(healthComponent);
+            if (collision.gameObject.TryGetComponent<HealthComponent>(out HealthComponent healthComponent))
+            {
+                Debug.Log("EXITED");
+                StopCoroutine(_entitiesInTrap[healthComponent]);
+                _entitiesInTrap.Remove(healthComponent);
+            }
         }
-    }
 
-    IEnumerator TrapDamageCoroutine(HealthComponent healthComp)
-    {
-        healthComp.GetHit(_damage);
-        Debug.Log("DMG");
-        while (true)
+        IEnumerator TrapDamageCoroutine(HealthComponent healthComp)
         {
-            yield return new WaitForSeconds(_delayBetweenHits);
             healthComp.GetHit(_damage);
             Debug.Log("DMG");
+            while (true)
+            {
+                yield return new WaitForSeconds(_delayBetweenHits);
+                healthComp.GetHit(_damage);
+                Debug.Log("DMG");
+            }
         }
     }
 }
