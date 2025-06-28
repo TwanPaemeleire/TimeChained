f35f36122b2be24f2a82b5e117f373f026b33a37
TwanPaemeleire <twan.paemeleire@gmail.com>
2025-06-28 17:25:46 +0200

    Made and applied a script that makes buttons smoothly change size when (un)selected.

--- Assets/Scripts/UI/ButtonVisualInteraction.cs
+++ Assets/Scripts/UI/ButtonVisualInteraction.cs
@@ -1,16 +1,49 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Assets.Scripts.UI
 {
-    public class ButtonVisualInteraction : MonoBehaviour
+    public class ButtonVisualInteraction : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        private Button _button;
+        [SerializeField] private Vector3 _maxScale;
+        [SerializeField] private float _timeToReachTargetScale = 1.0f;
 
-        private void Awake()
+        private Coroutine _scaleCoroutine;
+
+        public void OnPointerEnter(PointerEventData eventData)
         {
-            _button = GetComponent<Button>();
-            //_button.On
+            StartScaleCoroutine(_maxScale);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            StartScaleCoroutine(Vector3.one);
+        }
+
+        void StartScaleCoroutine(Vector3 targetScale)
+        {
+            if(_scaleCoroutine !=null)
+            {
+                StopCoroutine(_scaleCoroutine);
+            }
+            _scaleCoroutine = StartCoroutine(ChangeButtonScale(targetScale));
+        }
+
+        IEnumerator ChangeButtonScale(Vector3 targetScale)
+        {
+            Vector3 initialScale = transform.localScale;
+            float elapsedTime = 0f;
+
+            while (elapsedTime < _timeToReachTargetScale)
+            {
+                float newScale = Mathf.SmoothStep(initialScale.x, targetScale.x, elapsedTime / _timeToReachTargetScale);
+                transform.localScale = new Vector3(newScale, newScale, 1.0f);
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+            transform.localScale = targetScale;
         }
     }
 }
