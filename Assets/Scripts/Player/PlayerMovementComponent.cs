85800dfe1f8ca8d1b9ea3cb4ca6d597871358f8c
TwanPaemeleire <twan.paemeleire@gmail.com>
2025-06-29 23:17:19 +0200

    Implemented one-way platforms.

--- Assets/Scripts/Player/PlayerMovementComponent.cs
+++ Assets/Scripts/Player/PlayerMovementComponent.cs
@@ -1,4 +1,5 @@
 using Assets.Scripts.SharedLogic;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
@@ -12,13 +13,17 @@ namespace Assets.Scripts.Player
 
         private SpriteRenderer _spriteRenderer;
         private Rigidbody2D _rigidbody;
-        private float _inputMoveDirection;
+        private Vector2 _inputMoveDirection;
+        private Vector2 _previousInputMoveDirection;
         private bool _isGrounded;
         private bool _canDoubleJump;
         private bool _isJumping;
 
         private ShootComponent _shootComponent;
         private PlayerWeaponRotateComponent _weaponRotateComponent;
+
+        private GameObject _currentOneWayPlatform = null;
+        private BoxCollider2D _boxCollider;
 
         public UnityEvent OnMovementBegin = new UnityEvent();
         public UnityEvent OnMovementEnd = new UnityEvent();
@@ -30,6 +35,7 @@ namespace Assets.Scripts.Player
         void Start()
         {
             _rigidbody = GetComponent<Rigidbody2D>();
+            _boxCollider = GetComponent<BoxCollider2D>();
             _shootComponent = GetComponentInChildren<ShootComponent>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _weaponRotateComponent = GetComponentInChildren<PlayerWeaponRotateComponent>();
@@ -38,7 +44,7 @@ namespace Assets.Scripts.Player
         // Update is called once per frame
         void FixedUpdate()
         {
-            _rigidbody.linearVelocityX = _inputMoveDirection * _speed; // staying away from the y doesnt override gravity
+            _rigidbody.linearVelocityX = _inputMoveDirection.x * _speed; // staying away from the y doesnt override gravity
             _isGrounded = Physics2D.Raycast(transform.position, -transform.up, 0.55f, LayerMask.GetMask("Ground")); //TODO: for later maybe two for each side (maybe even getting it from collider width?)
             if(_isJumping && _isGrounded && _rigidbody.linearVelocityY <= 0f)
             {
@@ -49,15 +55,16 @@ namespace Assets.Scripts.Player
 
         public void Move(InputAction.CallbackContext context)
         {
+            _previousInputMoveDirection = _inputMoveDirection;
             if(context.canceled)
             {
-                _inputMoveDirection = 0.0f;
+                _inputMoveDirection = Vector2.zero;
                 OnMovementEnd?.Invoke();
             }
             else
             {
-                _inputMoveDirection = context.ReadValue<float>();
-                if(_inputMoveDirection < 0.0f)
+                _inputMoveDirection = context.ReadValue<Vector2>();
+                if(_inputMoveDirection.x < 0.0f)
                 {
                     _spriteRenderer.flipX = true;
                     _weaponRotateComponent.FlipLeft();
@@ -67,13 +74,18 @@ namespace Assets.Scripts.Player
                     _spriteRenderer.flipX = false;
                     _weaponRotateComponent.FlipRight();
                 }
-                if(context.started) OnMovementBegin?.Invoke();
+
+                if (_inputMoveDirection.x > 0.0f && _previousInputMoveDirection.x == 0.0f) OnMovementBegin?.Invoke();
+                if(_inputMoveDirection.y < 0.0f && !(_previousInputMoveDirection.y < 0.0f)) // Player wants to drop down through platform
+                {
+                     StartCoroutine(DisableOneWayCollisions());
+                }
             }
         }
 
         public void Jump(InputAction.CallbackContext context)
         {
-            if(context.started)
+            if (context.started)
             {
                 if (_isGrounded)
                 {
@@ -82,15 +94,38 @@ namespace Assets.Scripts.Player
                     _isJumping = true;
                     OnJumpBegin?.Invoke();
                 }
-                else if(_canDoubleJump)
+                else if (_canDoubleJump)
                 {
                     _rigidbody.linearVelocityY = 0; // otherwise jump immediately after first jump would go super high
                     _rigidbody.AddForce(transform.up * _jumpStrength, ForceMode2D.Impulse);
                     _canDoubleJump = false;
                 }
             }
+        }
 
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if(collision.gameObject.CompareTag("OneWayPlatform"))
+            {
+                _currentOneWayPlatform = collision.gameObject;
+            }
+        }
 
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("OneWayPlatform"))
+            {
+                _currentOneWayPlatform = null;
+            }
+        }
+
+        IEnumerator DisableOneWayCollisions()
+        {
+            BoxCollider2D oneWayPlatformCollider = _currentOneWayPlatform.GetComponent<BoxCollider2D>();
+            Physics2D.IgnoreCollision(_boxCollider, oneWayPlatformCollider);
+            _rigidbody.AddForce(new Vector2(0, -5.0f), ForceMode2D.Impulse);
+            yield return new WaitForSeconds(0.25f);
+            Physics2D.IgnoreCollision(_boxCollider, oneWayPlatformCollider, false);
         }
     }
 }
