39033750b0508e7aa78d5fea18537bbaa9909cbe
TwanPaemeleire <twan.paemeleire@gmail.com>
2025-07-02 22:48:21 +0200

    Implemented the boss health bar.

--- Assets/Scripts/UI/HealthDisplayHandler.cs
+++ Assets/Scripts/UI/HealthDisplayHandler.cs
@@ -6,20 +6,20 @@ namespace Assets.Scripts.UI
 {
     public class HealthDisplayHandler : MonoBehaviour
     {
-        [SerializeField] private HealthComponent _playerHealth;
-        private Slider _healthSlider;
+        [SerializeField] private HealthComponent _healthToDisplay;
+        private Image _fillImage;
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            _healthSlider = GetComponent<Slider>();
+            _fillImage = GetComponent<Image>();
             UpdateHealthDisplay();
-            _playerHealth.OnHit.AddListener(UpdateHealthDisplay);
-            _playerHealth.OnHeal.AddListener(UpdateHealthDisplay);
+            _healthToDisplay.OnHit.AddListener(UpdateHealthDisplay);
+            _healthToDisplay.OnHeal.AddListener(UpdateHealthDisplay);
         }
 
         void UpdateHealthDisplay()
         {
-            _healthSlider.value = _playerHealth.GetRemainingHealthPercentage();
+            _fillImage.fillAmount = _healthToDisplay.GetRemainingHealthPercentage();
         }
     }
 }
