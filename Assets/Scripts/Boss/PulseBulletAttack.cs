89f3b9026e684d24e4eb82a0e1de1405c53a866d
TwanPaemeleire <twan.paemeleire@gmail.com>
2025-07-02 11:19:00 +0200

    Added boss animations and started working on the boss arena.

---
+++ Assets/Scripts/Boss/PulseBulletAttack.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PulseBulletAttack : BossFightBaseAttack
+{
+    [SerializeField] private int _amountOfDirections = 6;
+
+    public override void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
