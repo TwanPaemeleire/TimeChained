563e1b7a0f122f236af94ff729516da9b69dac42
Thomas Ketelaere <thomas.ketelaere@outlook.be>
2025-07-03 18:18:16 +0200

    Boss Explosion SFX

--- Assets/Scripts/Boss/BossExplosion.cs
+++ Assets/Scripts/Boss/BossExplosion.cs
@@ -5,10 +5,16 @@ public class BossExplosion : MonoBehaviour
 {
     private Animator _animator;
     public UnityEvent<GameObject> OnFinishedPlaying = new UnityEvent<GameObject>();
+    public UnityEvent OnStartedPlaying = new UnityEvent();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        OnStartedPlaying?.Invoke();
     }
 
     // Update is called once per frame
